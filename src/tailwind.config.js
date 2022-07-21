57420749d1f725002f9bee1d1224d85aa29fe6d3
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-07-21 15:57:31 +0900

    微修正

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,20 +1,12 @@
 module.exports = {
-  mode: "jit",
-  darkMode: false, // 'media' or 'class',
-  purge: {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    options: {
-      // https://purgecss.com/safelisting.html#patterns
-      safelist: {
-        standard: [/^bg-/, /^text-/],
-      },
-    },
-  },
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
