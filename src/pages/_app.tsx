24f9387e732d5b07ff7b6af8c840e5701eb27323
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-07-24 17:29:41 +0900

    Auth0によるログイン認証実装#1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import "../styles/global.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  //ログイン後のリダイレクト先を指定
+  const redirectUrl = `${process.env["NEXT_PUBLIC_BASE_URL"]}/login`;
+  return (
+    <Auth0Provider
+      domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
+      clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]!}
+      audience={process.env["NEXT_PUBLIC_AUTH0_AUDIENCE"]!}
+      redirectUri={redirectUrl}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // export async function getStaticProps() {
 //   const allPostsData = getSortedPostsData();
@@ -22,31 +23,43 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ allPostsData }) {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <Layout home>
-      <p className="bg-blue-500 bg-opacity-100">Hello world!</p>
-      <h1 className="... rotate-180 transform">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <Link href={`/test`}>
-                <a>test</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <>
+      <Layout home>
+        <p className="bg-blue-500 bg-opacity-100">Hello world!</p>
+        <h1 className="... rotate-180 transform">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <Link href={`/test`}>
+                  <a>test</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Layout>
+      <div className="flex justify-center">
+        <button
+          className="rounded bg-gray-900 px-4 py-2 text-white hover:bg-gray-800"
+          onClick={() => loginWithRedirect()}
+        >
+          ログイン
+        </button>
+      </div>
+    </>
   );
 }
