54a9e622e9a78665992ff24f23b0a764f4df7cd9
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-14 16:19:41 +0900

    途中

--- src/pages/home/testpage/test1.jsx
+++ src/pages/home/testpage/test1.jsx
@@ -1,7 +1,11 @@
 import { useRouter } from "next/router";
+import { AuthContext } from "pages/_app";
+import { useContext } from "react";
 
 const Test1 = () => {
   const router = useRouter();
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
 
   const goToHome = () => {
     router.push("/home");
@@ -11,6 +15,7 @@ const Test1 = () => {
     <>
       <h1>test1</h1>
       <button onClick={goToHome}>goToHome</button>
+      <p onClick={goToHome}>{currentUser.email}</p>
     </>
   );
 };
